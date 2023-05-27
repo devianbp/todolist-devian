0926c11e14c40478dec7ad54719828e3962409db
devianbp <92678931+devianbp@users.noreply.github.com>
2023-05-27 18:05:47 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,74 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 // custom hooks
-import useLocalStorage from './hoooks/useLocalStorage'
+import useLocalStorage from "./hoooks/useLocalStorage";
 
 // costum components
-import CostumForm from './components/CustomForm'
-import EditForm from './components/EditForm'
-import TaskList from './components/TaskList'
+import CostumForm from "./components/CustomForm";
+import EditForm from "./components/EditForm";
+import TaskList from "./components/TaskList";
 
 function App() {
-  const [tasks, setTasks] = useLocalStorage('react-todo.task', []);
+  const [tasks, setTasks] = useLocalStorage("react-todo.task", []);
   const [previusFocusEL, setPreviusFocusEL] = useState(null);
   const [editedTask, setEditedTask] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
 
   const addTask = (task) => {
-    setTasks(prevState => [... prevState, task])
-  }
+    setTasks((prevState) => [...prevState, task]);
+  };
 
   const deleteTask = (id) => {
-    setTasks(prevState => prevState.filter(t => t.id !== id));
-  }
+    setTasks((prevState) => prevState.filter((t) => t.id !== id));
+  };
 
   const toggleTask = (id) => {
-    setTasks(prevState => prevState.map(t => (
-      t.id == id
-      ? { ... t, checked: !t.checked }
-      : t
-    )))
-  }
-
-  const updateTask = (task) => {
-    setTasks(prevState => prevState.map(t => (
-      t.id == task.id
-      ? { ... t, name: task.name }
-      : t
-    )))
-    closeEditMode();
-  }
+    setTasks((prevState) =>
+      prevState.map((t) => (t.id === id ? { ...t, checked: !t.checked } : t))
+    );
+  };
 
   const closeEditMode = () => {
     setIsEditing(false);
     previusFocusEL.focus();
-  }
+  };
+
+  const updateTask = (task) => {
+    setTasks((prevState) =>
+      prevState.map((t) => (t.id === task.id ? { ...t, name: task.name } : t))
+    );
+    closeEditMode();
+  };
 
   const enterEditMode = (task) => {
     setEditedTask(task);
     setIsEditing(true);
     setPreviusFocusEL(document.activeElement);
-  }
-  
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <header>
         <h1>Todo List</h1>
       </header>
-      {
-        isEditing && (
-          <EditForm  
-            editedTask={editedTask}
-            updateTask={updateTask}
-            closeEditMode={closeEditMode}
-          />
-        )
-      }
-      <CostumForm addTask={addTask}/>
+      {isEditing && (
+        <EditForm
+          editedTask={editedTask}
+          updateTask={updateTask}
+          closeEditMode={closeEditMode}
+        />
+      )}
+      <CostumForm addTask={addTask} />
       {tasks && (
-      <TaskList 
-      tasks={tasks}
-      deleteTask={deleteTask}
-      tonggleTask={toggleTask}
-      enterEditMode={enterEditMode}
-      />
-    )}
+        <TaskList
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
